3f556d4f2dd45766e0a45df1f440cd564e1fe0af
Arata Miyamoto <amiyamoto1441@gmail.com>
2021-07-31 17:12:56 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+build_image() {
+    docker build -t bfprt-qsort docker
+}
+
+run_container() {
+    CONTAINER_CMD=$1
+    docker run -v $(pwd):/root/bfprt-qsort -w=/root/bfprt-qsort --rm -it bfprt-qsort /bin/bash -c "${CONTAINER_CMD}"
+}
+
+if [ $# -lt 1 ]; then
+    2>&1 echo "usage: $0 <sort|profile|test>"
+    exit 1
+fi
+
+CMD=$1
+
+build_image
+
+if [ ${CMD} == "sort" ]; then
+    2>&1 echo "Not implemented."
+    exit 1
+elif [ ${CMD} == "profile" ]; then
+    CONTAINER_CMD=" \
+        mkdir -p build/ && \
+        cmake -S . -B build/ && \
+        build/samples/profile"
+elif [ ${CMD} == "test" ]; then
+    2>&1 echo "Not implemented."
+    exit 1
+fi
+
+run_container "${CONTAINER_CMD}"
